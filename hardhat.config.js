9cfb2ee9ed1429f935e145d467e25122fd7d150c
ashishiyer9 <32933445+ashishiyer9@users.noreply.github.com>
2024-08-28 23:26:36 +0000

    Sepolia config added

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,17 +2,14 @@ require('dotenv').config();
 require("@nomiclabs/hardhat-ethers");
 require('@nomiclabs/hardhat-waffle');
 
-const { INFURA_API_KEY, PRIVATE_KEY } = process.env;
-
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.24",
   defaultNetwork: "sepolia",
   networks: {
-    hardhat: {},
     sepolia: {
-      url: INFURA_API_KEY,
-      accounts: [`0x${PRIVATE_KEY}`]
+      url: `https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts: [`0x${process.env.PRIVATE_KEY}`]
     }
   },
 };
